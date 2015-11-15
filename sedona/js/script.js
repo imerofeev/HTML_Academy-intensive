39cb2a102b06e9e53cea0273984941c9004ece31
Ilya Erofeev <imerofeev@gmail.com>
2015-11-15 17:02:25 +0400

    доработка внешнего вида и декоративных элементов

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -1,20 +1,34 @@
-    var link = document.querySelector(".search-btn");
-    var popup = document.querySelector(".form");
-    var date = document.querySelector("[name=arrival]");
+var link = document.querySelector(".search-btn");
+var popup = document.querySelector(".form");
+var date = document.querySelector("[name=arrival]");
 
-    link.addEventListener("click", function(event) {
-      event.preventDefault();
-      popup.classList.toggle("show-form");
-      date.focus();
-
-
-    });
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+  popup.classList.toggle("show-form");
+  date.focus();
+});
 
 
-    window.addEventListener("keydown", function(event) {
-      if (event.keyCode == 27) {
-        if (popup.classList.contains("show-form")) {
-          popup.classList.remove("show-form");
-        }
-      }
-    });
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode == 27) {
+    if (popup.classList.contains("show-form")) {
+      popup.classList.remove("show-form");
+    }
+  }
+});
+
+
+function initialize() {
+  var mapOptions = {
+    zoom: 15,
+    center: new google.maps.LatLng(34.8546223,-111.8347477)
+  }
+  var map = new google.maps.Map(document.getElementById('map'),
+                                mapOptions);
+  var myLatLng = new google.maps.LatLng(34.8546223,-111.8347477);
+  var beachMarker = new google.maps.Marker({
+    position: myLatLng,
+    map: map
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
