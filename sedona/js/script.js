d31ede9933f560d9d385e5b42be2af41299bd7c2
Ilya Erofeev <imerofeev@gmail.com>
2015-11-17 01:01:34 +0400

    готовая верстка

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -17,18 +17,17 @@ window.addEventListener("keydown", function(event) {
   }
 });
 
+function initMap() {
+  var myLatLng = {lat: 34.865510, lng: -111.763656};
 
-function initialize() {
-  var mapOptions = {
-    zoom: 15,
-    center: new google.maps.LatLng(34.8546223,-111.8347477)
-  }
-  var map = new google.maps.Map(document.getElementById('map'),
-                                mapOptions);
-  var myLatLng = new google.maps.LatLng(34.8546223,-111.8347477);
-  var beachMarker = new google.maps.Marker({
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
     position: myLatLng,
-    map: map
+    map: map,
+    title: 'Sedona'
   });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
